d25f88272f328e7f9fe7fa8e0774fc0566b4d70e
Brian Kiarie Muigai <brian.muigai@student.moringaschool.com>
2021-12-14 06:22:08 +0300

    adding a Hero method defininf more the wrapper variables

--- src/test/java/HeroTest.java
+++ src/test/java/HeroTest.java
@@ -1,3 +1,7 @@
+import models.Hero;
+
+import java.util.ArrayList;
+
 public class HeroTest {
     private String mName;
     private int mAge;
@@ -6,4 +10,18 @@ public class HeroTest {
     private String mWeakness;
     private boolean mPublished;
     private String mSquadMembership;
+
+    public static ArrayList<Hero> mInstances = new ArrayList<Hero>();
+
+    public Hero(String name, int age, String specialPower, String weakness, String squadMembership) {
+        this.mName = name;
+        this.mAge = age;
+        this.mSpecialPower = specialPower;
+        this.mWeakness = weakness;
+        this.mPublished = false;
+        this.mSquadMembership = squadMembership;
+
+        mInstances.add(this);
+        this.id = mInstances.size();
+    }
 }
