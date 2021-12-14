17b6d6868874c395dcd7b8dd5078f0880a09ab03
Brian Kiarie Muigai <brian.muigai@student.moringaschool.com>
2021-12-14 06:26:42 +0300

    Making changes on the Herotest and the hero class

--- src/test/java/HeroTest.java
+++ src/test/java/HeroTest.java
@@ -1,27 +1,5 @@
-import models.Hero;
 
-import java.util.ArrayList;
 
 public class HeroTest {
-    private String mName;
-    private int mAge;
-    private int id;
-    private String mSpecialPower;
-    private String mWeakness;
-    private boolean mPublished;
-    private String mSquadMembership;
 
-    public static ArrayList<Hero> mInstances = new ArrayList<Hero>();
-
-    public Hero(String name, int age, String specialPower, String weakness, String squadMembership) {
-        this.mName = name;
-        this.mAge = age;
-        this.mSpecialPower = specialPower;
-        this.mWeakness = weakness;
-        this.mPublished = false;
-        this.mSquadMembership = squadMembership;
-
-        mInstances.add(this);
-        this.id = mInstances.size();
-    }
 }
