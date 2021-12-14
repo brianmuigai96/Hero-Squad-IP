a06060ae3728128fe854737c5c59e0f390563849
Brian Kiarie Muigai <brian.muigai@student.moringaschool.com>
2021-12-14 06:29:29 +0300

    checking if the object is created correctly

--- src/test/java/HeroTest.java
+++ src/test/java/HeroTest.java
@@ -1,5 +1,10 @@
-
+import org.junit.Before;
 
 public class HeroTest {
+//    check to see if an object is correctly created
+    @Before
+    public void tearDown(){
+        Hero.clearAllHeroes();
+    }
 
 }
