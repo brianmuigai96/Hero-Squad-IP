20de948687cc230d52b546393a8c56ea4b57d83b
Brian Kiarie Muigai <brian.muigai@student.moringaschool.com>
2021-12-14 06:19:06 +0300

    Declaring wrapper variables

--- src/test/java/HeroTest.java
+++ src/test/java/HeroTest.java
@@ -1,2 +1,9 @@
 public class HeroTest {
+    private String mName;
+    private int mAge;
+    private int id;
+    private String mSpecialPower;
+    private String mWeakness;
+    private boolean mPublished;
+    private String mSquadMembership;
 }
