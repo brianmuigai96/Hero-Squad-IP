fd3f5e1992f39a3443d337317d0cf5328e6d9163
Brian Kiarie Muigai <brian.muigai@student.moringaschool.com>
2021-12-14 06:34:27 +0300

    Testing to make sure all heroes can be added and all information is correct

--- src/test/java/HeroTest.java
+++ src/test/java/HeroTest.java
@@ -1,10 +1,26 @@
+import models.Hero;
 import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
 
 public class HeroTest {
 //    check to see if an object is correctly created
     @Before
     public void tearDown(){
         Hero.clearAllHeroes();
+    }
+    @Test
+    public void heroObjectIsCorrectlyCreated_true() {
+        Hero superHero = new Hero("Brian", 26, "Coder", "Patience", "master");
+        assertEquals(true, superHero instanceof Hero);
+    }
+    @Test
+    public void allHeroesContainsAllHeroes_true() {
+        Hero superHero = new Hero("Briam", 26, " Coder", "Patience", "master");
+        Hero secondSuperHero = new Hero("Twitty", 21, "Super", "Patience", "DC");
+        assertEquals(true, Hero.getAll().contains(superHero));
+        assertEquals(true, Hero.getAll().contains(secondSuperHero));
     }
 
 }
