c6952c3dfb05254f9594a37ad94f95e835532109
Brian Kiarie Muigai <brian.muigai@student.moringaschool.com>
2021-12-15 06:11:03 +0300

    assigning heroku port

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,2 +1,9 @@
 public class App {
+    static int definingHerokuPort() {
+        ProcessBuilder processBuilder = new ProcessBuilder();
+        if (processBuilder.environment().get("PORT") != null) {
+            return Integer.parseInt(processBuilder.environment().get("PORT"));
+        }
+        return 4567;
+    }
 }
